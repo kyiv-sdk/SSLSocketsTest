7ddd5b4cbc4fdd28fc688fee69e688ff3146991a
Oleksandr Hordiienko <ohord2@macmini65.ddns.softservecom.com>
2019-01-22 13:24:43 +0200

    Created SSLSockets framework; using it from both projects.

---
+++ IACReceiver/SSLSockets.framework/Headers/SSLSockets.h
@@ -0,0 +1,23 @@
+//
+//  SSLSockets.h
+//  SSLSockets
+//
+//  Created by Oleksandr Hordiienko on 1/22/19.
+//  Copyright © 2019 SoftServe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSLSockets.
+FOUNDATION_EXPORT double SSLSocketsVersionNumber;
+
+//! Project version string for SSLSockets.
+FOUNDATION_EXPORT const unsigned char SSLSocketsVersionString[];
+
+#import "SSLServerSocket.h"
+#import "SSLClientSocket.h"
+#import "SSLSocketHandler.h"
+#import "SSLSocketDelegate.h"
+#import "SSLSocketsManager.h"
+
+
