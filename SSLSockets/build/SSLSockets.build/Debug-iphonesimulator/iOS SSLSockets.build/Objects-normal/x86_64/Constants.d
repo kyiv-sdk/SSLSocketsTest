8ccbbcdc5c50f8971f8066a71210afe83e899c82
Oleksandr Hordiienko <ohord2@macmini65.ddns.softservecom.com>
2019-01-22 15:27:04 +0200

    Fix: Removed absolute paths, maked it compiled on each device.

--- SSLSockets/build/SSLSockets.build/Debug-iphonesimulator/iOS SSLSockets.build/Objects-normal/x86_64/Constants.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ohord2/Documents/GitHub/SSLSocketsTest/SSLSockets/SSLSockets/Sockets/BSDSockets\ C++/Constants.m \
-  /Users/ohord2/Documents/GitHub/SSLSocketsTest/SSLSockets/build/SSLSockets.build/Debug-iphonesimulator/iOS\ SSLSockets.build/module.modulemap
--- SSLSockets/build/SSLSockets.build/Debug-iphonesimulator/iOS SSLSockets.build/Objects-normal/x86_64/SSLSockets_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/ohord2/Documents/GitHub/SSLSocketsTest/SSLSockets/build/SSLSockets.build/Debug-iphonesimulator/iOS\ SSLSockets.build/DerivedSources/SSLSockets_vers.c
