65f2a9dcbe231a0e3f256b0bd802f741a65d29a0
Oleksandr Hordiienko <ohord2@macmini65.ddns.softservecom.com>
2019-02-01 09:08:16 +0200

    Feature: RCManager app initial commit.

---
+++ RCManager/RCManager/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  RCManager
+//
+//  Created by Oleksandr Hordiienko on 2/1/19.
+//  Copyright © 2019 SoftServe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ RCManager/RCManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RCManager
+//
+//  Created by Oleksandr Hordiienko on 2/1/19.
+//  Copyright © 2019 SoftServe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
